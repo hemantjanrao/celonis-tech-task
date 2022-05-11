f3b41c6d5c1171ecceb26ec4057191fbe4f1a3a6
Hemant Janrao <hemantjanrao@gmail.com>
2022-05-11 13:45:23 +0200

    updating base test

--- src/main/java/com/celonis/driver/BrowserFactory.java
+++ src/main/java/com/celonis/driver/BrowserFactory.java
@@ -3,22 +3,18 @@ package com.celonis.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.config.DriverManagerType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public enum BrowserFactory {
     CHROME{
         @Override
         public WebDriver createDriver() {
-            WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
-            return new ChromeDriver();
+            return WebDriverManager.getInstance(DriverManagerType.CHROME).create();
         }
     },
     FIREFOX{
         @Override
         public WebDriver createDriver() {
-            WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
-            return new FirefoxDriver();
+            return WebDriverManager.getInstance(DriverManagerType.FIREFOX).create();
         }
     };
     public abstract WebDriver createDriver();
